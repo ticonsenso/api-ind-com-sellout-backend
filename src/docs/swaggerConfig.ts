c7011177a965f5dae264f4d26ab5c7a64c9f9da2
jpsolanoc <jpsolanoc@gmail.com>
2026-02-13 11:44:58 -0500

    Ajuste en maestros con perido e prod id

--- src/docs/swaggerConfig.ts
+++ src/docs/swaggerConfig.ts
@@ -13,7 +13,7 @@ const options: swaggerJsdoc.Options = {
     openapi: "3.0.0",
     info: {
       title: "API SELLOUT (Consenso)",
-      version: "1.1.4",
+      version: "1.1.5",
       description: "Proyecto de manejo de datos sellout",
     },
 
