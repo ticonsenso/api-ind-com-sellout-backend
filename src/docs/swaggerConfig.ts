086ef033e66b7061f477da90f2000453d48fa3c3
jpsolanoc <jpsolanoc@gmail.com>
2026-02-18 17:22:05 -0500

    bugfix: Ajuste en sincronizacion

--- src/docs/swaggerConfig.ts
+++ src/docs/swaggerConfig.ts
@@ -13,7 +13,7 @@ const options: swaggerJsdoc.Options = {
     openapi: "3.0.0",
     info: {
       title: "API SELLOUT (Consenso)",
-      version: "1.1.7",
+      version: "1.1.8",
       description: "Proyecto de manejo de datos sellout",
     },
 
