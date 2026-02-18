5fb635dbb9901c5bfd38f8b9688496c8c4194fdd
jpsolanoc <jpsolanoc@gmail.com>
2026-02-18 09:36:16 -0500

    feature: obtener excel para subir al sistema sellout

--- src/docs/swaggerConfig.ts
+++ src/docs/swaggerConfig.ts
@@ -13,7 +13,7 @@ const options: swaggerJsdoc.Options = {
     openapi: "3.0.0",
     info: {
       title: "API SELLOUT (Consenso)",
-      version: "1.1.5",
+      version: "1.1.6",
       description: "Proyecto de manejo de datos sellout",
     },
 
